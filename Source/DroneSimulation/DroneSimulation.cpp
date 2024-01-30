956121e2cb4ce27fb21342497e68ea6b18b5329f
MagicStellar <singhs.sumit27@gmail.com>
2024-01-30 19:22:17 +0530

    Intial Commit - Files Added

---
+++ Source/DroneSimulation/DroneSimulation.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DroneSimulation.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DroneSimulation, "DroneSimulation" );
+ 
---
+++ dronerun.bat
@@ -0,0 +1,2 @@
+@echo off
+start "3Guns - Client" "D:\Engines\Unreal\UE_5.2\Engine\Binaries\Win64\UnrealEditor.exe" "%CD%\DroneSimulation.uproject" -game -ResX=906 -ResY=512 -WINDOWED -log -localtesting
